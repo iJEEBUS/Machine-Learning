29084fad553f57c58b6fa0b8e86494515dfd1192
Ron Rounsifer <ronrounsifer@me.com>
2019-02-14 21:04:12 -0500

    Save point. Still need to fix classification.

--- project 2/naive_bayes.py
+++ project 2/naive_bayes.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import csv
+from math import log, exp
 
 # loading training data into data frame
 all_data = pd.read_csv("./data/training.txt", sep="\n")
-#all_data.head()
+all_data = all_data.sample(100)
 
 '''
 1.0 Collect all words occurring in the sample documents
@@ -11,7 +12,7 @@ all_data = pd.read_csv("./data/training.txt", sep="\n")
 print("Collecting all distinct words...")
 # collect all distinct words
 vocab = set()
-num_rows = len(all_data.index)
+num_rows = len(all_data.index) 
 
 for row in range(num_rows):
     for post in all_data.iloc[row]:
@@ -82,36 +83,47 @@ for word in vocab:
 
 '''
 3.0 Classification - classify new data
+# TODO need to get this working
 '''
 print("Classifying new data...")
 total_correct_classifications = 0
 
-new_data = pd.read_csv('./data/testing.txt', sep='\n')
-num_rows = len(new_data.index)
+#new_data = pd.read_csv('./data/training.txt', sep='\n')
+#num_rows = len(new_data.index) // 10
 
-# (class, post)
 for row in range(num_rows):
-    max_probability = ('', 0.0) # (class, probability)
-    probability = 0
 
-    for post in new_data.iloc[row]:
+    for post in all_data.iloc[row]:
+        class_probabilities = {}
+        max_probability = ('', None) # (class, probability)
+        probability = 0.0
         correct_classification = post.split()[0]
         only_content = ' '.join(post.split()[1:])
+        print(len(class_probabilities.keys()))
     
     for classification in text.keys():
         for word in only_content.split():
             if word in word_occurrence_estimate.keys():
-                probability += log(word_occurrence_estimate[word][classification]) # might need to use log here
+                probability += log(word_occurrence_estimate[word][classification]) # use log to account for underflow
             else:
-                prob = (1 / (len(text[classification]) + len(vocab)))
-                probability += log(prob)
-        if exp(probability) > exp(max_probability[1]):
+                prob = log(1) - log((len(text[classification]) + len(vocab)))
+                probability += prob
+        class_probabilities[classification] = probability
+    print('\n')
+    # set the max probability of each posts classification
+    for classification in class_probabilities.keys():
+        print(classification, class_probabilities[classification])
+        if (max_probability[1] == None):
             max_probability = (classification, probability)
-      
+        elif (class_probabilities[classification] < max_probability[1]):
+            #print("New highest prob")
+            max_probability = (classification, probability)
+            
+    # counting correct assignments        
     if correct_classification == max_probability[0]:
         total_correct_classifications += 1
         
-    print("Correct classification: %s  ::  %s" % (correct_classification, max_probability[0]))
+    print("%s  ::  %s" % (correct_classification, max_probability[0]))
 
 accuracy = (total_correct_classifications / num_rows)
 print("Accuracy: %s" % (accuracy))
